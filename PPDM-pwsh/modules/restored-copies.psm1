fe6b139b31d7d903c9e1f74c49a2d262cf44ccd6
Karsten Bott <karsten.bott@labbuildr.com>
2023-06-29 11:17:55 +0200

    added documetation

--- PPDM-pwsh/modules/restored-copies.psm1
+++ PPDM-pwsh/modules/restored-copies.psm1
@@ -1151,7 +1151,63 @@ function Restore-PPDMFileFLR_copies {
   }
 }
 
+<#
+.Synopsis
+Restores MSAPPSAgent based SQL Database Backups
+.Description
+Centralized Restore of MSAPPSAgent based SQL Database Backups to Same or different Host
+.Example
+# Set SQ Hostname(s) , Instance and Database
+RestoreFromHost = "sqlsinglenode.dpslab.home.labbuildr.com"
+$RestoreToHost_Name = "sqlsinglenode.dpslab.home.labbuildr.com"
+$AppServerName = "MSSQLDPSLAB"
+$DataBaseName = "AdventureWorks2019"
 
+### Get all Windows Hosts with MSSQL Apps
+
+$Appfilter = 'attributes.appHost.appServerTypes eq "MSSQL" and not (lastDiscoveryStatus eq "DELETED") and details.appHost.os lk "WINDOWS" and details.appHost.phase eq "NONE"'
+Get-PPDMhosts -filter $AppFilter
+
+### Read our Restore Host
+
+$RestoreHostFilter = 'attributes.appHost.appServerTypes eq "MSSQL" and not (lastDiscoveryStatus eq "DELETED") and details.appHost.os lk "WINDOWS" and details.appHost.phase eq "NONE" and hostname eq "' + $RestoreToHost_Name + '"'
+$RestoreToHost = Get-PPDMhosts -filter $RestoreHostFilter
+$RestoreToHost
+
+### Read the Asset to restore to identify the Asset Copies
+
+$RestoreAssetFilter = 'type eq "MICROSOFT_SQL_DATABASE" and protectionStatus eq "PROTECTED" and details.database.clusterName eq "' + $RestoreFromHost + '"' + ' and details.database.appServerName eq "' + $AppServerName + '"'
+$RestoreAssets = Get-PPDMAssets -Filter $RestoreAssetFilter
+$RestoreAssets = $RestoreAssets | Where-Object name -Match $DataBaseName
+# Optionally, look at the CopyMap
+# $Copymap=$RestoreAssets | Get-PPDMcopy_map
+
+### Selecting the Asset Copy to Restore 
+write-host "Selecting Asset-copy for $DataBaseName"
+$myDate = (get-date).AddDays(-2)
+$usedate = get-date $myDate -Format yyyy-MM-ddThh:mm:ssZ
+$RANGE_FILTER = 'startTime ge "' + $usedate + '"state eq "IDLE"'
+# $RestoreAssets | Get-PPDMassetcopies -filter $RANGE_FILTER
+$RestoreAssetCopy = $RestoreAssets | Get-PPDMassetcopies -filter $RANGE_FILTER | Select-Object -First 1
+
+### Run the Restore :-)  
+```Powershell
+$Parameters = @{
+  HostID                  = $RestoreToHost.id 
+  appServerId             = $RestoreAssets.details.database.appServerId
+  copyObject              = $RestoreAssetCopy
+  enableDebug             = $false
+  disconnectDatabaseUsers = $true
+  restoreType             = "TO_ALTERNATE" 
+  CustomDescription       = "Restore from Powershell"
+  Verbose                 = $false
+}
+$Restore = Restore-PPDMMSSQL_copies @Parameters
+
+### Monitor the Restore
+$Restore | Get-PPDMRestored_copies
+$Restore | Get-PPDMactivities
+#>
 function Restore-PPDMMSSQL_copies {
   [CmdletBinding()]
   [Alias('Restore-PPDMDDB_MSSQL')]
--- example_scripts/flr_mssql_db.ps1
+++ example_scripts/flr_mssql_db.ps1
@@ -1,57 +1,61 @@
 
 
-
 $RestoreFromHost = "sqlsinglenode.dpslab.home.labbuildr.com"
-
 $RestoreToHost_Name = "sqlsinglenode.dpslab.home.labbuildr.com"
-
 $AppServerName = "MSSQLDPSLAB"
 $DataBaseName = "AdventureWorks2019"
 
-connect-pPDMapiEndpoint -trustCert -PPDM_API_BaseURI ppdmavs01.edub.csc
-$RestoreFromHost = "WhqNQLqCLJ2yw.edub.csc"
-$RestoreToHost_Name = "bM10k2Mjn53aO.edub.csc"
 
 
 
-### Get all linux Hosts with FLR Agent
+
+### Get all Windows Hosts with MSSQL Apps
+
 $Appfilter = 'attributes.appHost.appServerTypes eq "MSSQL" and not (lastDiscoveryStatus eq "DELETED") and details.appHost.os lk "WINDOWS" and details.appHost.phase eq "NONE"'
 Get-PPDMhosts -filter $AppFilter
+
+### Read our Restore Host
+
 $RestoreHostFilter = 'attributes.appHost.appServerTypes eq "MSSQL" and not (lastDiscoveryStatus eq "DELETED") and details.appHost.os lk "WINDOWS" and details.appHost.phase eq "NONE" and hostname eq "' + $RestoreToHost_Name + '"'
 $RestoreToHost = Get-PPDMhosts -filter $RestoreHostFilter
+$RestoreToHost
+
+
+### Read the Asset to restore to identify the Asset Copies
 
 $RestoreAssetFilter = 'type eq "MICROSOFT_SQL_DATABASE" and protectionStatus eq "PROTECTED" and details.database.clusterName eq "' + $RestoreFromHost + '"' + ' and details.database.appServerName eq "' + $AppServerName + '"'
-$RestoreAssetFilter = 'protectionStatus eq "PROTECTED" and details.fileSystem.hostName eq  "' + $RestoreFromHost + '"'
-
 $RestoreAssets = Get-PPDMAssets -Filter $RestoreAssetFilter
 $RestoreAssets = $RestoreAssets | Where-Object name -Match $DataBaseName
+# Optionally, look at the CopyMap
+# $Copymap=$RestoreAssets | Get-PPDMcopy_map
 
+### Selecting the Asset Copy to Restore 
 
 
 write-host "Selecting Asset-copy for $DataBaseName"
-$myDate = (get-date).AddDays(-1)
+$myDate = (get-date).AddDays(-2)
 $usedate = get-date $myDate -Format yyyy-MM-ddThh:mm:ssZ
 $RANGE_FILTER = 'startTime ge "' + $usedate + '"state eq "IDLE"'
-$RestoreAssets | Get-PPDMassetcopies -filter $RANGE_FILTER
+# $RestoreAssets | Get-PPDMassetcopies -filter $RANGE_FILTER
 $RestoreAssetCopy = $RestoreAssets | Get-PPDMassetcopies -filter $RANGE_FILTER | Select-Object -First 1
 
-
-
-
-
-
-
+### Run the Restore :-)  
+```Powershell
 $Parameters = @{
-  HostID               = $RestoreToHost.id 
-  appServerId          = $RestoreAssets.details.database.appServerId
-  ids                  = $RestoreAssetCopy.id
-  assetName            = $RestoreAssetCopy.assetName
-  enableDebug          = $false
+  HostID                  = $RestoreToHost.id 
+  appServerId             = $RestoreAssets.details.database.appServerId
+  copyObject              = $RestoreAssetCopy
+  enableDebug             = $false
   disconnectDatabaseUsers = $true
-  restoreType          = "TO_ALTERNATE" 
-  CustomDescription    = "Restore from Powershell"
-  Verbose              = $false
+  restoreType             = "TO_ALTERNATE" 
+  CustomDescription       = "Restore from Powershell"
+  Verbose                 = $false
 }
 
 $Restore = Restore-PPDMMSSQL_copies @Parameters
+
+### Monitor the Restore
+
+$Restore | Get-PPDMRestored_copies
+$Restore | Get-PPDMactivities
 
