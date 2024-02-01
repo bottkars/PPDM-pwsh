33ffc4505300f0afc913cb7064147c6458d89975
Karsten.Bott <Karsten.Bott@bottnet.de>
2024-02-01 15:55:23 +0100

    results

--- PPDM-pwsh/modules/protection_policies.psm1
+++ PPDM-pwsh/modules/protection_policies.psm1
@@ -355,7 +355,7 @@ function Start-PPDMprotection {
 
       switch ($PsCmdlet.ParameterSetName) {
         default {
-          write-host $response.results
+          write-host $response
         } 
       }   
     }
