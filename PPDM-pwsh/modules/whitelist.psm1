27f4ef6e528a2d251bf576203104441005d4ae5e
Karsten Bott <karsten.bott@labbuildr.com>
2024-02-05 09:49:27 +0100

    19.16-pre

--- PPDM-pwsh/modules/whitelist.psm1
+++ PPDM-pwsh/modules/whitelist.psm1
@@ -119,7 +119,7 @@ function Update-PPDMWhitelist {
     Process {
         $URI = "$myself/$id"
         $myDate = (get-date).AddDays($DaysToExpire)
-        $usedate = get-date $myDate -Format yyyy-MM-ddThh:mm:ss.ffffZ
+        $usedate = get-date $myDate -Format yyyy-MM-ddThh:mm:ss.ffffFFFFZ
         $body = @{}         
 #        $body.Add('expiresAt', $usedate )       
         $body.Add('state', $state)
